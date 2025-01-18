048b4841966a331e66c956a9b86c9373ab9f3fbc
sivakusi12 <sivakusi12@gmail.com>
2025-01-18 19:27:38 +0530

    sbvfrebtr4t

--- tools/scripts/git-push-code.bat
+++ tools/scripts/git-push-code.bat
@@ -1,14 +1,30 @@
 @ECHO OFF
 
+SET /p db_export = "Do you want to export complete DB? : "
+IF /i "%db_export%" == "y" OR "%db_export%" == "Y" GOTO EXPORT_DB
+GOTO COMMIT
+
+:EXPORT_DB
 ECHO "EXPORT DATABASE"
 cd ..\\..\\
 mysqldump -u root -p --databases mk2s_fms  > .\\migrations\\initialize-base-db.sql
 ECHO "EXPORT COMPLETE"
 
 
+:COMMIT
+SET /p commit_message="What do you want the commit message to be? : "
+ECHO "commit message "
+/i echo Committing with message: "%commit_message%"
+if "%commit_message%"=="" (
+    echo Commit message cannot be empty. Please try again.
+    goto end
+)
+
+
+
 ECHO "GIT COMMIT BACKEND"
 git add .
-git commit -m "Added more features related to encryption decryptoion"
+git commit -m "%commit_message%"
 git push
 ECHO "GIT PUSHED BACKEND"
 
@@ -16,7 +32,8 @@ cd ../fms_fe
 
 ECHO "GIT COMMIT FRONT END"
 git add .
-git commit -m "Added more features related to encryption decryptoion"
+git commit -m "%commit_message%"
 git push
 ECHO "GIT PUSHED FRONT END"
 
+:end
