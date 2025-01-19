3d78fb959a59c071f1b271f7ab521bc8bdaf969b
sivakusi12 <sivakusi12@gmail.com>
2025-01-19 09:48:56 +0530

    erbere

--- tools/scripts/git-push-code.bat
+++ tools/scripts/git-push-code.bat
@@ -51,7 +51,7 @@ SET /P UI_DEPLOY="Do you want to deploy, Please provide y for Yes? "
 IF /I "%UI_DEPLOY%" NEQ "y" GOTO ENDING
 
 SET /P UI_RDB="Do you want to recreate DB, Please provide y for Yes? "
-ssh mk2s.ovh "cd /var/app/fms_be/tools/scripts/deployments && bash ./deploy.sh" << "@
+ssh mk2s.ovh "cd /var/app/fms_be/tools/scripts/deployments && bash ./deploy.sh" < "@
 %UI_RDB%
 @
 "
